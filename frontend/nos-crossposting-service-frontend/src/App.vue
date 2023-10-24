a9d640057eace4820600b198779771c43c3bf7d3
boreq <boreq@0x46.net>
2023-10-24 02:05:55 +0200

    Fonts, fix background again

--- frontend/nos-crossposting-service-frontend/src/App.vue
+++ frontend/nos-crossposting-service-frontend/src/App.vue
@@ -8,11 +8,47 @@
 </template>
 
 <style lang="scss">
+@font-face {
+  font-family: 'Clarity City';
+  font-style: normal;
+  font-weight: 400;
+  font-display: swap;
+  src: url("./assets/fonts/claritycity/ClarityCity-Regular.otf") format('opentype');
+}
+
+@font-face {
+  font-family: 'Clarity City';
+  font-style: normal;
+  font-weight: 500;
+  font-display: swap;
+  src: url("./assets/fonts/claritycity/ClarityCity-Medium.otf") format('opentype');
+}
+
+@font-face {
+  font-family: 'Clarity City';
+  font-style: normal;
+  font-weight: 600;
+  font-display: swap;
+  src: url("./assets/fonts/claritycity/ClarityCity-SemiBold.otf") format('opentype');
+}
+
+@font-face {
+  font-family: 'Clarity City';
+  font-style: normal;
+  font-weight: 700;
+  font-display: swap;
+  src: url("./assets/fonts/claritycity/ClarityCity-Bold.otf") format('opentype');
+}
+
 html, body {
   padding: 0;
   margin: 0;
   color: #fff;
-  height: 100%;
+  min-height: 100vh;
+
+  font-family: 'Clarity City', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 html {
@@ -27,10 +63,6 @@ body {
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-
   .wrapper {
     padding: 4em;
   }
